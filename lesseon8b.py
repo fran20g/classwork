da2f8120585adfd5bc0a1194095f026b25df6c32
Francis <56949105+fran20g@users.noreply.github.com>
2019-11-14 11:20:03 +0600

    Add files via upload

---
+++ lesseon8b.py
@@ -0,0 +1,20 @@
+print("=====MSin Menu====")
+print("1.Add")
+print("2.List")
+print("3.Edit")
+print("Enter your Selection : ")
+
+choice = input()
+
+
+if choice =="1":
+    print("Add New record")
+elif choice =="2":
+    print("List Record")
+elif choice =="3":
+    print("Edit Record")
+elif choice =="4":
+    print("Exit from program")
+
+else:
+    print("Invalid Selection")
