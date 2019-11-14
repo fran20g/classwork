76858bf95017395b3cc6771d84c833488f3da4ce
Francis <56949105+fran20g@users.noreply.github.com>
2019-11-14 11:19:36 +0600

    Add files via upload

---
+++ lesseon8a.py
@@ -0,0 +1,28 @@
+"""
+ init variable
+ while condition:
+     statements
+      ....
+"""
+
+i=1
+while i<=5:
+    print (i,end='\t')
+    i=i+1  
+
+print("\n")
+print(50*"=")     
+
+i=10
+while True:
+    if i>=20:
+     break
+    print (i,end='\t')
+    i=i+2 
+
+print("\n")
+print(50*"=")     
+
+
+
+       
