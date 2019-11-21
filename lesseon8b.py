4e9367aae79ee2ebdcc7653c6a9c4b017d311016
Francis <56949105+fran20g@users.noreply.github.com>
2019-11-21 11:11:38 +0600

    Add files via upload

--- lesseon8b.py
+++ lesseon8b.py
@@ -1,20 +1,21 @@
-print("=====MSin Menu====")
-print("1.Add")
-print("2.List")
-print("3.Edit")
-print("Enter your Selection : ")
+while True :
+    print("=====Main Menu====")
+    print("1.Add")
+    print("2.List")
+    print("3.Edit")
+    print("Enter your Selection : ",end="")
 
-choice = input()
+    choice = input()
 
 
-if choice =="1":
-    print("Add New record")
-elif choice =="2":
-    print("List Record")
-elif choice =="3":
-    print("Edit Record")
-elif choice =="4":
-    print("Exit from program")
-
-else:
-    print("Invalid Selection")
+    if choice =="1":
+        print("Add New record")
+    elif choice =="2":
+        print("List Record")
+    elif choice =="3":
+        print("Edit Record")
+    elif choice =="4":
+        print("Exit from program")
+        break
+    else:
+        print("Invalid Selection")
---
+++ lesseon9a.py
@@ -0,0 +1,58 @@
+from os import system
+system('cls')
+while True :
+    print("=====Main Menu====")
+    print("1.Add")
+    print("2.List")
+    print("3.Edit")
+    print("4.Exit")
+    print("Enter your Selection : ",end="")
+
+    choice = input()
+
+
+    if choice =="1":
+        print("Add New record")
+    elif choice =="2":
+        print("List Record")
+    elif choice =="3":
+        print("Edit Record")
+    elif choice =="4":
+        print("Exit from program")
+        break
+    else:
+        print("Invalid Selection")
+
+
+    input("Press ENTER to continue...")
+     #system('cls')    
+
+
+
+
+while True :
+    print("=====Main Menu====")
+    print("1.Add")
+    print("2.List")
+    print("3.Edit")
+    print("4.Exit")
+    print("Enter your Selection : ",end="")
+
+    choice = input()
+
+
+    if choice =="1":
+        print("Add New record")
+    elif choice =="2":
+        print("List Record")
+    elif choice =="3":
+        print("Edit Record")
+    elif choice =="4":
+        print("Exit from program")
+        break
+    else:
+        print("Invalid Selection")
+
+
+    input("Press ENTER to continue...")
+     #system('cls')    
