8d0e8a3f133b22faea07b92c556dca9d18d4f8d2
Francis <56949105+fran20g@users.noreply.github.com>
2019-11-24 12:20:56 +0600

    Add files via upload

---
+++ lesseon10method.py
@@ -0,0 +1,33 @@
+
+
+def display():
+    print("This is Line 1") 
+    print("This is Line 2") 
+    print("This is Line 3") 
+    print("This is Line 4") 
+    print("This is Line 5") 
+    print("Prepared By XYZ")   
+
+
+
+
+def Show(x):
+    print("My Name Is %s" %(x))
+    print("%s is good boy" %(x))
+    print("%s ar kau nai" %(x))
+    print("%s kau nai" %(x))
+    
+display()  
+
+
+Show('abir')
+
+
+
+def friends(x,y):
+    print("%s and %s are good friend" % (x,y))
+  
+  
+
+
+friends("Faa","Jabad")
